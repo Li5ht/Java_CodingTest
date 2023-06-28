a882cb9fae78dfb079fcc04d902141d1b9b250fb
Li5ht <bell1234795@gmail.com>
2023-06-28 23:30:52 +0900

    Day03 Commit

---
+++ Day03.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+interface Day03 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+       int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+       while(input[0] != 0 && input[1] != 1) {
+           if (input[0] / input[1] > 0 && input[0] % input[1] == 0) {
+               System.out.println("multiple");
+           } else if (input[1] / input[0] > 0 && input[1] % input[0] == 0) {
+               System.out.println("factor");
+           } else {
+               System.out.println("neither");
+           }
+           input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+       }
+    }
+}
