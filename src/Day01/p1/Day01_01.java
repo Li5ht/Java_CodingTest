d605afb2b43a97faaef0ee53313b5f8e0c855196
Li5ht <bell1234795@gmail.com>
2023-07-31 18:19:57 +0900

    Day01 commit

--- src/Day01/p1/Day01_01.java
+++ src/Day01/p1/Day01_01.java
@@ -6,12 +6,12 @@ public class Day01_01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        //System.out.print("태국 나이를 입력하시오 : ");
+        System.out.print("태국 나이를 입력하시오 : ");
         int taiLandYr = sc.nextInt();
 
-        //int koreanYr = taiLandYr - 543;
-        //System.out.println("한국 나이 : " + koreanYr);
-        System.out.print(taiLandYr - 543);
+        int koreanYr = taiLandYr - 543;
+        System.out.println("한국 나이 : " + koreanYr);
+        //System.out.print(taiLandYr - 543);
 
         sc.close();
     }
