1edf5f69caf4d1ab18470bdc1ae13bd6dcbc1e89
Li5ht <bell1234795@gmail.com>
2023-07-31 15:29:44 +0900

    InteliJ commit

---
+++ src/Day01/Day01.java
@@ -0,0 +1,18 @@
+package Day01;
+
+import java.util.Scanner;
+
+public class Day01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //System.out.print("태국 나이를 입력하시오 : ");
+        int taiLandYr = sc.nextInt();
+
+        //int koreanYr = taiLandYr - 543;
+        //System.out.println("한국 나이 : " + koreanYr);
+        System.out.print(taiLandYr - 543);
+
+        sc.close();
+    }
+}
