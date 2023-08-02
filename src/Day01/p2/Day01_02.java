59145e615c94c45caa399a8c494ac12db988d824
Li5ht <bell1234795@gmail.com>
2023-08-02 23:47:43 +0900

    Day01_02 edit

--- src/Day01/p2/Day01_02.java
+++ src/Day01/p2/Day01_02.java
@@ -6,5 +6,20 @@ public class Day01_02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        //출력 : 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+        System.out.println((a+b)%c);
+        System.out.println(((a%c) + (b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c) * (b%c))%c);
+
+        sc.close();
+
+
+
+
     }
 }
