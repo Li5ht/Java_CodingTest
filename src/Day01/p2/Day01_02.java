0723e0bf1bcbc1cae8f561b6481f0194858099bc
Li5ht <bell1234795@gmail.com>
2023-08-02 23:33:54 +0900

    Day01_02 commit

--- src/Day01/p2/Day01_02.java
+++ src/Day01/p2/Day01_02.java
@@ -1,7 +1,10 @@
 package Day01.p2;
 
+import java.util.Scanner;
+
 public class Day01_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-
-
+    }
 }
