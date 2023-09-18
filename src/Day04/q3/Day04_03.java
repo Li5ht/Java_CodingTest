af382cdd11df5774b3d0a3614975ae601823f9be
Li5ht <bell1234795@gmail.com>
2023-09-19 02:12:49 +0900

    Day4 edit

--- src/Day04/q3/Day04_03.java
+++ src/Day04/q3/Day04_03.java
@@ -1,4 +1,17 @@
 package Day04.q3;
 
+import java.util.Scanner;
+
 public class Day04_03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int total = sc.nextInt();
+        int num = sc.nextInt();
+
+
+
+        sc.close();
+    }
 }
+
