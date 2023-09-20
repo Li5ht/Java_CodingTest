42748b27d51aeb6d3c2c3e20d745955a474d3159
Li5ht <bell1234795@gmail.com>
2023-09-20 23:32:12 +0900

    Day4 edit

--- src/Day04/q3/Day04_03.java
+++ src/Day04/q3/Day04_03.java
@@ -6,12 +6,25 @@ public class Day04_03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int total = sc.nextInt();
-        int num = sc.nextInt();
+        int buyTotal = sc.nextInt();
+        int buyNum = sc.nextInt();
+        int calTotal  = 0;
 
+        int[][] receiptList = new int[buyNum][2];
 
+        for (int i = 0; i < buyNum; i ++){
+            for (int j = 0; j < 2; j++) {
+                receiptList[i][j] = sc.nextInt();
+            }
+            calTotal = calTotal + receiptList[i][0] *receiptList[i][1];
+        }
 
-        sc.close();
+        if (buyTotal == calTotal)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+
+        sc.close(); 
     }
 }
 
