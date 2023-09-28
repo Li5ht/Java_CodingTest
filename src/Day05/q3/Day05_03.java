aedcaa6994085f1f333ed2b5aadeec4165da2851
Li5ht <bell1234795@gmail.com>
2023-09-28 23:30:23 +0900

    Day5 edit

--- src/Day05/q3/Day05_03.java
+++ src/Day05/q3/Day05_03.java
@@ -6,6 +6,22 @@ public class Day05_03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+
+        int[] a = new int[n];
+        int[] b = new int[n];
+        int[] sum = new int[n];
+
+        for (int i=0; i<n; i++){
+            a[i] = sc.nextInt();
+            b[i] = sc.nextInt();
+            sum[i] = a[i] + b[i];
+        }
+
+        for (int i=0; i<n; i++){
+            System.out.println("Case "+(i+1)+": "+sum[i]);
+        }
+
         sc.close();
     }
 }
