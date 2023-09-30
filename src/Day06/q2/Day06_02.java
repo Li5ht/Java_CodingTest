889216d441592cac661831535bc61907d5c50382
Li5ht <bell1234795@gmail.com>
2023-09-30 23:42:37 +0900

    Day6 edit

--- src/Day06/q2/Day06_02.java
+++ src/Day06/q2/Day06_02.java
@@ -6,6 +6,15 @@ public class Day06_02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
         sc.close();
     }
 }
--- src/Day06/q3/Day06_03.java
+++ src/Day06/q3/Day06_03.java
@@ -1,4 +1,23 @@
 package Day06.q3;
 
+import java.util.Scanner;
+
 public class Day06_03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                for (int k = N-1 ; k >=0; k--){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
 }
