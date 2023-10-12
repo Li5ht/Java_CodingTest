5a0b6307b9534cb2501e4a68f0004b4347707872
Li5ht <bell1234795@gmail.com>
2023-10-12 14:31:41 +0900

    Day 7~8 edit

--- src/Day08/q1/Day08_01.java
+++ src/Day08/q1/Day08_01.java
@@ -1,4 +1,32 @@
 package Day08.q1;
 
+import java.util.Scanner;
+
 public class Day08_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int[] A = new int[n];
+
+        //int[] small = new int[n];
+        //int add = 0;
+
+        for (int i = 0; i < n ; i++){
+            A[i] = sc.nextInt();
+           /* if (A[i] < x){
+                small[add] = A[i];
+                add++;
+            }*/
+        }
+
+        for (int i = 0; i < n ; i++){
+            if (A[i] < x) {
+                System.out.print(A[i] + " ");
+            }
+        }
+
+        sc.close();
+    }
 }
