5f1e05ab1d972d8cc206b9468bf81572d6e25822
Yeji Park <bell1234795@gmail.com>
2023-11-01 23:41:44 +0900

    Day08 Edit

--- src/Day08/q3/Day08_03.java
+++ src/Day08/q3/Day08_03.java
@@ -1,27 +1,8 @@
 package Day08.q3;
 
 public class Day08_03 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int[] A = new int[n];
-
-        int max = 0, maxArrayNum = 0;
-
-        for (int i = 0; i < n ; i++){
-            A[i] = sc.nextInt();
-        }
-
-        for (int i = 0; i < n ; i++){
-            if (A[i] < max ) {
-                max = A[i];
-                maxArrayNum = i;
-            }
-        }
-
-        System.out.println(max + " " + maxArrayNum);
-
-        sc.close();
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = {};
+        return answer;
     }
 }
