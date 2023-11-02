d8cdbc2434053f357ae78265e97192cb65851157
Yeji Park <bell1234795@gmail.com>
2023-11-02 12:23:48 +0900

    Day09 edit

--- src/Day09/q1/Day09_01.java
+++ src/Day09/q1/Day09_01.java
@@ -5,5 +5,10 @@ import java.util.Scanner;
 public class Day09_01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.println(a);
+
+        sc.close();
     }
 }
