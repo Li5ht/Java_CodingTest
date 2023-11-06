16e25c6b0ec5556ae2bbb6a1abb69b24f733abea
Yeji Park <bell1234795@gmail.com>
2023-11-06 11:47:21 +0900

    Programmers Day02 add

--- src/main/java/com/codingTest/Programmers/Day02/q2/Day02_02.java
+++ src/main/java/com/codingTest/Programmers/Day02/q2/Day02_02.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 
 public class Day02_02 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
     }
 }
--- src/main/java/com/codingTest/Programmers/Day02/q3/Day02_03.java
+++ src/main/java/com/codingTest/Programmers/Day02/q3/Day02_03.java
@@ -5,5 +5,10 @@ import java.util.Scanner;;
 public class Day02_03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        String str = a + b;
+
+        System.out.println(str);
     }
 }
