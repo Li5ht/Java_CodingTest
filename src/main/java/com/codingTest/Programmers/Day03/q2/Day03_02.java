ff9656d3ad05ea86ce44bc21a876e200741afa98
Yeji Park <bell1234795@gmail.com>
2023-11-09 23:47:42 +0900

    Programmers Day03 edit

--- src/main/java/com/codingTest/Programmers/Day03/q2/Day03_02.java
+++ src/main/java/com/codingTest/Programmers/Day03/q2/Day03_02.java
@@ -2,7 +2,11 @@ package com.codingTest.Programmers.Day03.q2;
 
 public class Day03_02 {
     public String solution(String my_string, String overwrite_string, int s) {
-        String answer = "";
+        String answer = my_string.substring(0, s) + overwrite_string;
+
+        if(my_string.length() > answer.length()) {
+            answer += my_string.substring(answer.length());
+        }
         return answer;
     }
 }
