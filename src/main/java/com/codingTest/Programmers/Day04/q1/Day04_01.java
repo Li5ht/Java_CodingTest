519f88bbd076fa901df1463f39623002c79cb363
Yeji Park <bell1234795@gmail.com>
2023-11-10 23:39:46 +0900

    Programmers Day04 edit

--- src/main/java/com/codingTest/Programmers/Day04/q1/Day04_01.java
+++ src/main/java/com/codingTest/Programmers/Day04/q1/Day04_01.java
@@ -3,6 +3,10 @@ package com.codingTest.Programmers.Day04.q1;
 public class Day04_01 {
     public String solution(String[] arr) {
         String answer = "";
+
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
         return answer;
     }
 }
