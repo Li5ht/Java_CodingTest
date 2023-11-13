01ca47439aa04eff879c4c39e42dd3b8851b1dd5
Yeji Park <bell1234795@gmail.com>
2023-11-13 23:42:01 +0900

    Programmers Day04 edit

--- src/main/java/com/codingTest/Programmers/Day04/q3/Day04_03.java
+++ src/main/java/com/codingTest/Programmers/Day04/q3/Day04_03.java
@@ -1,4 +1,15 @@
 package com.codingTest.Programmers.Day04.q3;
 
 public class Day04_03 {
+    public int solution(int a, int b) {
+
+        String ab = String.valueOf(a) + String.valueOf(b);
+        String ba = String.valueOf(b) + String.valueOf(a);
+
+        if (Integer.parseInt(ab) >= Integer.parseInt(ba)) {
+            return Integer.parseInt(ab);
+        } else {
+            return Integer.parseInt(ba);
+        }
+    }
 }
