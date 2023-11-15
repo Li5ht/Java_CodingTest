d9a163356a966d6aaafc932785a1dc4102d75858
Yeji Park <bell1234795@gmail.com>
2023-11-15 23:06:45 +0900

    PCCE Q1 add

---
+++ src/main/java/com/codingTest/PCCP/Q1/Q1.java
@@ -0,0 +1,14 @@
+package com.codingTest.PCCP.Q1;
+
+import java.util.*;
+
+public class Q1 {
+    private static class Point{
+        public final long x, y;
+
+        private Point(long x, long y){
+            this.y = y;
+            this.x = x;
+        }
+    }
+}
