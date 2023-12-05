cf383219bd4d4fcfbe166f6ee8ef9b46aaf40629
Yeji Park <bell1234795@gmail.com>
2023-12-05 12:28:45 +0900

    PCCP Q2 edit

--- src/main/java/com/codingTest/PCCP/Q2/Q2.java
+++ src/main/java/com/codingTest/PCCP/Q2/Q2.java
@@ -1,24 +1,16 @@
 package com.codingTest.PCCP.Q2;
 
-import com.codingTest.PCCP.Q1.Q1;
+import java.util.*;
 
 public class Q2 {
-    private static class Point{
-        public final long x, y;
+    public int[] solution (int n) {
+        Scanner sc = new Scanner(System.in);
+        //int n = sc.nextInt();
+        int x  = -1, y = 0 ;
 
-        private Point(long x, long y){
-            this.y = y;
-            this.x = x;
-        }
-    }
-
-    private Q1.Point intersection(long a1, long b1, long c1, long a2, long b2, long c2){
-        double x = (double) (b1 * c2 - b2 * c1) / (a1 * b2 - b2 * b1);
-        double y = (double) (a2 * c1 - a1 * c2) / (a1 * b2 - b2 * b1);
-
-        if (x % 1 != 0 || y % 1 != 0)
-            return null;
-
-        return new Q1.Point((long)x, (long)y);
+       // int[][] arr = new int[n][n];
+        sc.close();
+        //int[] answer = {};
+      //  return answer;
     }
 }
