194c5a37c6773fb5c60ea6a77a009480b7eed55f
Yeji Park <89853141+Li5ht@users.noreply.github.com>
2024-05-05 23:34:34 +0900

    OOPL Day05 change

--- src/main/java/com/codingTest/OOPL/Day05/q2/Day05_02.java
+++ src/main/java/com/codingTest/OOPL/Day05/q2/Day05_02.java
@@ -1,4 +1,20 @@
 package com.codingTest.OOPL.Day05.q2;
 
+import com.codingTest.OOPL.Day05.q1.Date;
+
 public class Day05_02 {
+    public static void main(String[] args){
+        com.codingTest.OOPL.Day05.q1.Date d1 = new com.codingTest.OOPL.Day05.q1.Date();
+        com.codingTest.OOPL.Day05.q1.Date d2 = new Date();
+
+        d1.setYear(2010);
+        d1.setMonth(8);
+        d1.setDay(31);
+        System.out.println(d1.toString());
+
+        d2.setYear(1998);
+        d2.setMonth(9);
+        d2.setDay(15);
+        System.out.println(d2.toString());
+    }
 }
