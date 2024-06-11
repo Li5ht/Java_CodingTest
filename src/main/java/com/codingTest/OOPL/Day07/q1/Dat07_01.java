cbe2da3d4cd2a9286eb6fd692b28b92620373e32
Yeji Park <89853141+Li5ht@users.noreply.github.com>
2024-06-11 14:56:38 +0900

    OOPL Day07_01 edit

--- src/main/java/com/codingTest/OOPL/Day07/q1/Dat07_01.java
+++ src/main/java/com/codingTest/OOPL/Day07/q1/Dat07_01.java
@@ -1,4 +1,40 @@
 package com.codingTest.OOPL.Day07.q1;
 
+import java.util.*;
+
 public class Dat07_01 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        BodyData me = new BodyData();
+        BodyData you = new BodyData();
+
+        System.out.print("내 키를 입력하세요: ");
+        me.setHeight(sc.nextInt());
+        System.out.print("내 몸무게를 입력하세요: ");
+        me.setWeight(sc.nextInt());
+
+        System.out.print("당신의 키를 입력하세요: ");
+        you.setHeight(sc.nextInt());
+        System.out.print("당신의 키를 입력하세요: ");
+        you.setWeight(sc.nextInt());
+
+        if (me.isTallerThan(you))
+            System.out.println("키는 내가 더 큽니다.");
+        else
+            System.out.println("나와 당신의 키는 같습니다");
+
+        if (me.needDiet())
+            System.out.println("나는 다이어트가 필요합니다.");
+        else
+            System.out.println("나는 다이어트가 필요하지 않습니다.");
+
+        if (you.needDiet())
+            System.out.println("당신은 다이어트가 필요합니다.");
+        else
+            System.out.println("당신은 다이어트가 필요하지 않습니다.");
+
+        sc.close();
+
+    }
 }
