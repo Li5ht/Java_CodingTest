14acf53ede9c5185b996118ef01017becd30a290
Yeji Park <89853141+Li5ht@users.noreply.github.com>
2024-07-30 23:47:40 +0900

    OOPL Day08s edit

---
+++ src/main/java/com/codingTest/OOPL/Day08/q1/Student.java
@@ -0,0 +1,39 @@
+package com.codingTest.OOPL.Day08.q1;
+
+public class Student {
+    private String major;
+    private long sId;
+
+    public String getMajor() { return major;}
+    public void setMajor(String major) { this.major = major;}
+    public long getsId() { return sId;}
+    public void setsId(long sId) { this.sId = sId;}
+
+    public String printSafely(){
+        return toString()+ ", 학번: "+sId+", 전공: "+ major+"\n";
+    }
+
+    public Student(){
+        super();
+        this.sId = -1;
+        this.major = "모름";
+    }
+
+    public Student(String name){
+        super(name);
+        this.sId = -1;
+        this.major = "모름";
+    }
+
+    public Student(String name, int age, long sId){
+        super(name, age);
+        this.sId = sId;
+        this.major = "모름";
+    }
+
+    public Student(String name, int age, String address, long sId, String major){
+        super(name, age, address);
+        this.sId = sId;
+        this.major = "모름";
+    }
+}
