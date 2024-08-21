b0a6394eaaa4aa487d5d511a9124e762fe89ef09
Yeji Park <89853141+Li5ht@users.noreply.github.com>
2024-08-21 23:33:58 +0900

    OOPL DAY10s edit

--- src/main/java/com/codingTest/OOPL/Day10/q1/Day10_01.java
+++ src/main/java/com/codingTest/OOPL/Day10/q1/Day10_01.java
@@ -1,4 +1,7 @@
 package com.codingTest.OOPL.Day10.q1;
 
 public class Day10_01 {
+    public static void main(String[] args){
+
+    }
 }
--- src/main/java/com/codingTest/OOPL/Day10/q2/Day10_02.java
+++ src/main/java/com/codingTest/OOPL/Day10/q2/Day10_02.java
@@ -1,4 +1,7 @@
 package com.codingTest.OOPL.Day10.q2;
 
 public class Day10_02 {
+    public static void main(String[] args){
+
+    }
 }
